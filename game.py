ace5c6fc6838cfcbcc2c6200d20fc6dd44d2c9e2
Zhao Yiming <zhao.yiming423@gmail.comm>
2021-05-01 17:59:35 +0200

    continue minimax player

--- game.py
+++ game.py
@@ -4,6 +4,9 @@ import os
 from client import sendJSON, receiveJSON, NotAJSONObject, fetch
 from threading import Timer
 import random
+import copy
+import time
+from collections import defaultdict
 
 def send(information):
     return sendJSON(s,information)
@@ -23,38 +26,241 @@ def getEnnemy(current):
     symbols = ['W', 'B']
     return symbols[current]
 
-def checkMarble(board,pos):
-    li,ci=pos[0],pos[1]
-    direct=[[-1,0],[1,0],[-1,-1],[1,1],[0,1],[0,-1]]
-    voisins=[]
-    for elem in direct:
-        try:
-            voisins.append(board[li+elem[0]][ci+elem[1]])
-        except:pass
-    if 'E' in voisins:
-        return True
+def opposite(current):
+	if current==1:
+		return 0
+	return 1
+
+def computeMarbles(board,color):
+	marbles=0
+	i=0
+	while i<9:
+		for state in range(len(board[i])):
+			if board[i][state]==color and OnBoard([i,state])==True:
+				marbles+=1
+		i+=1
+	return marbles
+
+def winner(board,current):
+	color=getColor(current)
+	ennemy=getEnnemy(current)
+	if computeMarbles(board,color)<=8:
+		return ennemy
+	if computeMarbles(board,ennemy)<=8:
+		return color
+	return None
+
+def utility(board,current):
+	theWinner=winner(board,current)
+	if theWinner is None:
+		return 0
+	if theWinner == getColor(current):
+		return 1
+	return -1
+
+def OnBoard(pos):
+	l,c=pos[0],pos[1]
+	if min(pos)<0:
+		return False
+	if max(pos)>8:
+		return False
+	if abs(c-l)>=5:
+		return False
+	return True
+
+def moves(board,color,ennemy):
+	moveOne=[]
+	posMarbles=[]
+	moveTrain=[]
+	i=0
+	while i<9:
+		for state in range(len(board[i])):
+			if board[i][state]==color and OnBoard([i,state])==True:
+				posMarbles.append((i,state))
+		i+=1
+	for direction in directions:
+		for marble in posMarbles:
+			li,ci=marble[0],marble[1]
+			l,c=directions[direction][0],directions[direction][1]
+			try:
+				if board[li+l][ci+c]=='E': #anvance 1 pion dans le vide
+					moveOne.append([(li,ci),direction])
+				if board[li+l][ci+c]==ennemy and OnBoard([li+l,ci+c])==True: 
+					if board[li+2*l][ci+2*c]=='E' or OnBoard([li+2*l,ci+2*c])==False:#pousse 2 contre 1
+						if board[li-l][ci-c]==color and OnBoard([li-l,ci-c])==True:
+							moveTrain.append([(li,ci),(li-l,ci-c),direction])
+							if board[li-2*l][ci-2*c]==color and OnBoard([li-2*l,ci-2*c])==True:#pousse 3 contre 1
+								moveTrain.append([(li,ci),(li-l,ci-c),(li-2*l,ci-2*c),direction])
+					if board[li+2*l][ci+2*c]==ennemy and OnBoard([li+2*l,ci+2*c])==True: #pousse 3 contre 2
+						if board[li+3*l][ci+3*c]=='E' or OnBoard([li+3*l,ci+3*c])==False:
+							if board[li-l][ci-c]==color and board[li-2*l][ci-2*c]==color:
+								if OnBoard([li-l,ci-c])==True and OnBoard([li-2*l,ci-2*c])==True:
+									moveTrain.append([(li,ci),(li-l,ci-c),(li-2*l,ci-2*c),direction])
+			except:pass
+	for move in moveOne:
+		li,ci,direction = move[0][0],move[0][1],move[1]
+		lf,cf=li-directions[direction][0],ci-directions[direction][1]
+		ld,cd=lf-directions[direction][0],cf-directions[direction][1]
+		try:
+			if board[lf][cf]==color and OnBoard([lf,cf])==True:
+				moveTrain.append([(li,ci),(lf,cf),direction])
+				if board[ld][cd]==color and OnBoard([ld,cd])==True:
+					moveTrain.append([(li,ci),(lf,cf),(ld,cd),direction])
+		except:pass
+	moveAll=moveTrain+moveOne
+	return random.sample(moveAll,len(moveAll))
+
+def moveOneMarble(board,move,color):
+	li,ci = move[0][0],move[0][1]
+	direction=move[1]
+	ld,cd=li+directions[direction][0],ci+directions[direction][1]
+	try:
+		destStatus=board[ld][cd]
+	except:
+		destStatus='X'
+	board=copy.copy(board)
+	board[li]=copy.copy(board[li])
+	board[li][ci]='E'
+	if destStatus=='E':
+		board[ld]=copy.copy(board[ld])
+		board[ld][cd]=color
+	return board
+
+def isFree(board,pos):
+	if OnBoard(pos):
+		l,c =pos[0],pos[1]
+		return board[l][c]=='E'
+	else:
+		return True
+
+def MoveMarbles(board,marbles,direction,color):
+	for pos in marbles:
+		board=moveOneMarble(board,[(pos),direction],color)
+	return board
+
+def moveMarbleTrain(board,move,color,ennemy):
+	direction=move[-1]
+	marbles= move[0:-1]
+	if direction in ['E','SE','SW']:
+		marbles=sorted(marbles,key=lambda L:-(L[0]*9+L[1]))
+	else:
+		marbles=sorted(marbles,key=lambda L:L[0]*9+L[1])
+	lf,cf=marbles[0][0]+directions[direction][0],marbles[0][1]+directions[direction][1]
+	push=[]
+	while not isFree(board,[lf,cf]):
+		push.append([lf,cf])
+		lf,cf=lf+directions[direction][0],cf+directions[direction][1]
+	board=MoveMarbles(board,list(reversed(push))+marbles,direction,color)
+	return board
+
+def apply(board,move,color,ennemy):
+	state=list(board)
+	if len(move)==2:
+		return moveOneMarble(state,move,color)
+	return moveMarbleTrain(state,move,color,ennemy)
+
+def timeit(fun):
+	def wrapper(*args,**kwargs):
+		start=time.time()
+		res=fun(*args,**kwargs)
+		return res
+	return wrapper
+
+@timeit
+def next(board,player,fun):
+	_,move=fun(board,player)
+	return move
+
+def gameOver(board,current):
+	if winner(board,current) is not None:
+		return True
+	return False
+
+def lineValue(state, player):
+	color=getColor(player)
+	ennemy=getEnnemy(player)
+	mesMarbles=computeMarbles(state,color)
+	ennemyMarbles=computeMarbles(state,ennemy)
+
+	if mesMarbles>ennemyMarbles:
+		return mesMarbles-ennemyMarbles
+	if mesMarbles==ennemyMarbles:
+		return 0
+	return -mesMarbles+ennemyMarbles
+
+
+def heuristic(state, player):
+	color=getColor(player)
+	if gameOver(state,player):
+		theWinner = winner(state,player)
+		if theWinner is None:
+			return 0
+		if theWinner == color:
+			return 9
+		return -9
+	res = lineValue(state, player)
+	return res
+	
+def negamaxWithPruningIterativeDeepening(state, player, timeout=2.8):
+	cache = defaultdict(lambda : 0)
+	def cachedNegamaxWithPruningLimitedDepth(state, player, depth, alpha=float('-inf'), beta=float('inf')):
+		color=getColor(player)
+		ennemy=getEnnemy(player)
+		over = gameOver(state,player)
+		if over ==True or depth == 0:
+			res = -heuristic(state, player), None, over
+		else:
+			theValue, theMove, theOver = float('-inf'), None, True
+			possibilities = [(move, apply(state, move,color,ennemy)) for move in moves(state,color,ennemy)]
+			possibilities.sort(key=lambda poss: cache[tuple(poss[0][1])])
+			for move, successor in reversed(possibilities):
+				value, _, over = cachedNegamaxWithPruningLimitedDepth(successor, opposite(player), depth-1, -beta, -alpha)
+				theOver = theOver and over
+				if value > theValue:
+					theValue, theMove = value, move
+				alpha = max(alpha, theValue)
+				if alpha >= beta:
+					break
+			res = -theValue, theMove, theOver
+		cache[tuple(state[0])] = res[0]
+		return res
+
+	value, move = 0, None
+	depth = 1
+	start = time.time()
+	over = False
+	while value > -9 and time.time() - start < timeout and not over :
+		value, move, over = cachedNegamaxWithPruningLimitedDepth(state, player, depth)
+		depth += 1
+	return value, move
+
+def run(fun,board,current):
+	move=None
+	start = time.time()
+	while time.time() - start < 2.8:
+		color=getColor(current)
+		ennemy=getEnnemy(current)
+		move=next(board,current,fun)
+	return move
+		
+
+def listenServer():
+    sock=socket.socket()
+    sock.bind(('0.0.0.0',port))
+    sock.listen()
+    server, address = sock.accept()
+    data=receiveJSON(server)
+    if data['request']=='ping':
+        return sendJSON(server,{'response':'pong'})
+    if data['request']=='play':
+        move=run(negamaxWithPruningIterativeDeepening,data['state']['board'],data['state']['current'])
+        return sendJSON(server,{'response':'move','move':{'marbles':move[0:-1],'direction':move[-1]},'message':"random player"})
     else:
-        return False
+        print(data)
 
-def randomMarble(color,board,posMarbles):
-    value=random.randrange(len(posMarbles))
-    pos=posMarbles[value]
-    if checkMarble(board,pos)==True:
-        return pos
-    else:
-        return randomMarble(color,board,posMarbles[:value]+posMarbles[value+1:])
 
-def choixMarble(color,board):
-    posMarbles=[]
-    i=0
-    while i<9:
-        for state in range(len(board[i])):
-            if board[i][state]==color:
-                posMarbles.append([i,state])
-        i+=1
-    return randomMarble(color,board,posMarbles)
 
-def checkDirection(marble,board,direction):
+if __name__ == '__main__':
     directions = {
 	'NE': [-1,0],
 	'SW': [1,0],
@@ -63,80 +269,6 @@ def checkDirection(marble,board,direction):
 	 'E': [0,1],
 	 'W': [0,-1]
     }
-    li,ci=marble[0],marble[1]
-    try:
-        lf,cf=li+directions[direction][0],ci+directions[direction][1]
-        if board[lf][cf]=='E':
-            return True
-        else:
-            return False
-    except:return False
-
-def choixDirection(marble,board,listeDirections=['NE','SW','NW','SE','E','W']):
-    val=random.randrange(len(listeDirections))
-    direction=listeDirections[val]
-    if checkDirection(marble,board,direction)==True:
-        return direction
-    else:
-        return choixDirection(marble,board,listeDirections[:val]+listeDirections[val+1:])
-
-def winner(board,current):
-    color=getColor(current)
-    ennemy=getEnnemy(current)
-    mesMarbles=0
-    ennemyMarbles=0
-    i=0
-    while i<9:
-        for state in range(len(board[i])):
-            if board[i][state]==color:
-                mesMarbles+=1
-            if board[i][state]==ennemy:
-                ennemyMarbles+=1
-        i+=1
-    if mesMarbles<=8:
-        return ennemy
-    if ennemyMarbles<=8:
-        return color
-    return None
-
-def utility(board,current):
-    theWinner=winner(board,current)
-    if theWinner is None:
-        return 0
-    if theWinner == getColor(current):
-        return 1
-    return -1
-
-def gameOver(board,current):
-    if winner(board,current) is not None:
-        return True
-    return False
-
-
-
-def listenServer():
-    sock=socket.socket()
-    sock.bind(('0.0.0.0',port))
-    #try:
-    sock.listen()
-    server, address = sock.accept()
-    data=receiveJSON(server)
-    if data['request']=='ping':
-        return sendJSON(server,{'response':'pong'})
-    if data['request']=='play':
-        color= getColor(data['state']['current'])
-        marble=choixMarble(color,data['state']['board'])
-        marbles=(marble,)
-        direction=choixDirection(marble,data['state']['board'])
-        return sendJSON(server,{'response':'move','move':{'marbles':marbles,'direction':direction},'message':"random player"})
-    else:
-        print(data)
-    #except:
-        #pass
-
-
-
-if __name__ == '__main__':
     port=int(sys.argv[1])
     s=socket.socket()
     s.connect((socket.gethostname(),3000))
